59bcde07b0bc17a8c3eae876ce9c734aae4e25ce
Awalkurnia <akaaa@MacBook-Air-Awalkurnia.local>
2025-06-28 18:01:05 +0700

    update asset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Sistem</title>
     
-    <link rel="stylesheet" href="{{ secure_asset('secure_assets/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <style>
